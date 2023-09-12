a0589311ea0e427696593a05bf02877566847ec2
Pedro Almeida <pedrog-almeida@hotmail.com>
2023-09-12 02:05:48 -0300

    finishing navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -35,11 +38,35 @@ const NavBar = () => {
 
       <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
-          <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
             {link}
           </li>
         ))}
       </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center 
+        absolute top-0 left-0 w-full h-screen bg-gradient-to-b 
+        from-black to-gray-800 text-gray-500">
+        {links.map(({ id, link }) => (
+          <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">
+            {link}
+          </li>
+        ))}
+      </ul>
+      )}
+      
+
     </div>
   );
 };
