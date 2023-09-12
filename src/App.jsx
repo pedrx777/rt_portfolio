046c996909c06bdbc424ab54705ee9fdfe661ea2
Pedro Almeida <pedrog-almeida@hotmail.com>
2023-09-12 02:41:51 -0300

    adding home

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./components/Home"
 import NavBar from "./components/NavBar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+import HeroImage from "../assets/eu.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black 
+    via-black to-gray-800"
+    >
+      <div
+        className="max-w-screen-lg mx-auto flex flex-col 
+        items-center justify-center h-full px-4 md:flex-row"
+      >
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">Desenvolvedor Frontend</h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Recentemente fiz uma transição em minha carreira para o
+            desenvolvimento Frontend como Freelancer e tenho projetos meus tanto
+            concluídos como em andamento em meu GitHub. Minha especialidade
+            inclui React, HTML, CSS, JavaScript, Node e Tailwind CSS.
+          </p>
+
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2
+            flex items-center rounded-md bg-gradient-to-r
+            from-cyan-500 to-blue-500 cursor-pointer">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div>
+            <img src={HeroImage} alt="Minha foto" className="rounded-2xl mx-auto w-2/3 md:w-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
