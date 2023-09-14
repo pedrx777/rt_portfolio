20598c7fbce6ec211351dfcf8fd89d9390124861
Pedro Goncalves de Almeida <2301675@SALAS.AULAS>
2023-09-13 21:37:35 -0300

    feat(experience): starting experience page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About"
+import Experience from "./components/Experience"
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
 import Portfolio from "./components/Portfolio"
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Portfolio />
+      <Experience />
 
       <SocialLinks />
     </div>
