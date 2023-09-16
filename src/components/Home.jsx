14148db2d56d3facb2cf3fe2af9ac090eaeebd28
Pedro Goncalves de Almeida <2301675@SALAS.AULAS>
2023-09-15 21:52:16 -0300

    fix: responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         className="max-w-screen-lg mx-auto flex flex-col 
         items-center justify-center h-full px-4 md:flex-row"
       >
-        <div className="flex flex-col justify-center h-full">
+        <div className="mt-14 md:mt-0 flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">Desenvolvedor Frontend</h2>
           <p className="text-gray-500 py-4 max-w-md">
             Recentemente fiz uma transição em minha carreira para o
